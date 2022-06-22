83d02bee790e2b6bc92100f6d37b8259dac15098
s-kakol <kakolsylwester@gmail.com>
2022-06-22 20:44:15 +0200

    Feat: Add env config

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,12 @@
+import { Logger } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const PORT = parseInt(process.env.PORT, 10) || 3001;
+
+  Logger.log(`Server started on port ${PORT}`, `Server`);
+  await app.listen(PORT);
 }
 bootstrap();
