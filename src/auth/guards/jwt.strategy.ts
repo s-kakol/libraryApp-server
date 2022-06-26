62e4ee1ec143f7f7d4149550c6a021d3e7250c39
s-kakol <kakolsylwester@gmail.com>
2022-06-26 15:06:13 +0200

    Feat: Add JWT guard & strategy

---
+++ src/auth/guards/jwt.strategy.ts
@@ -0,0 +1,23 @@
+import { Injectable, Logger } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import { PassportStrategy } from '@nestjs/passport';
+import { ExtractJwt, Strategy } from 'passport-jwt';
+import { AuthService } from '../auth.service';
+
+@Injectable()
+export class JwtStrategy extends PassportStrategy(Strategy) {
+  constructor(
+    private authService: AuthService,
+    private configService: ConfigService,
+  ) {
+    super({
+      jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+      ignoreExpiration: false,
+      secretOrKey: configService.get('JWT_SECRET'),
+    });
+  }
+
+  async validate(payload: any) {
+    return { ...payload.user };
+  }
+}
