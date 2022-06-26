06f26da855dd14faf914c775195ec27d67f61e00
s-kakol <kakolsylwester@gmail.com>
2022-06-26 15:18:22 +0200

    Fix: Remove unnecessary imports from jwt.strategy

--- src/auth/guards/jwt.strategy.ts
+++ src/auth/guards/jwt.strategy.ts
@@ -1,15 +1,11 @@
-import { Injectable, Logger } from '@nestjs/common';
+import { Injectable } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 import { PassportStrategy } from '@nestjs/passport';
 import { ExtractJwt, Strategy } from 'passport-jwt';
-import { AuthService } from '../auth.service';
 
 @Injectable()
 export class JwtStrategy extends PassportStrategy(Strategy) {
-  constructor(
-    private authService: AuthService,
-    private configService: ConfigService,
-  ) {
+  constructor(private configService: ConfigService) {
     super({
       jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
       ignoreExpiration: false,
